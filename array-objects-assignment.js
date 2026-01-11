aa2304dfb85519559771a989d069ca765495ed6e
Daniel Akiije <danakj010@gmail.com>
2026-01-11 16:05:08 +0100

    initial

---
+++ array-objects-assignment.js
@@ -0,0 +1,34 @@
+const students = [
+  {
+    id: 1,
+    name: "Wale Adenuga",
+    age: 22,
+    grades: [65, 72, 91],
+  },
+  {
+    id: 2,
+    name: "Femi Olushola",
+    age: 20,
+    grades: [52, 68, 84],
+  },
+  {
+    id: 3,
+    name: "Chisom Oluchi",
+    age: 23,
+    grades: [69, 82, 95],
+  },
+  {
+    id: 4,
+    name: "Victor Osayi",
+    age: 26,
+    grades: [59, 77, 65],
+  },
+  {
+    id: 5,
+    name: "Gideon Emmanuel",
+    age: 24,
+    grades: [78, 87, 93],
+  },
+];
+
+console.log(students);
