aad7fdcd44e77e4d9f4f0af318778c84d7af7003
Daniel Akiije <danakj010@gmail.com>
2026-01-12 23:17:08 +0100

    add features

--- array-objects-assignment.js
+++ array-objects-assignment.js
@@ -1,3 +1,4 @@
+// QUESTION 1
 const students = [
   {
     id: 1,
@@ -32,3 +33,57 @@ const students = [
 ];
 
 console.log(students);
+
+// QUESTION 2
+
+function calculateAverage(grades) {
+  const sum = grades.reduce((total, grade) => total + grade, 0);
+  const average = sum / grades.length;
+  return average.toFixed(2);
+}
+
+const studentsWithAverage = students.map((student) => {
+  return {
+    ...student,
+    average: calculateAverage(student.grades),
+  };
+});
+
+console.log(studentsWithAverage);
+
+// QUESTION 3
+
+function getPassingStudents(students) {
+  return students.filter((student) => {
+    return parseFloat(student.average) >= 60;
+  });
+}
+const passing = getPassingStudents(studentsWithAverage);
+console.log(passing);
+
+// QUESTION 4
+
+function calculateClassAverage(students) {
+  const totalSum = students.reduce((total, student) => {
+    return total + parseFloat(student.average);
+  }, 0);
+  const classAvg = totalSum / students.length;
+  return classAvg.toFixed(2);
+}
+const classAvg = calculateClassAverage(studentsWithAverage);
+console.log(classAvg);
+
+//  QUESTION 5
+
+function findStudentById(students, id) {
+  const foundStudent = students.find((student) => {
+    return student.id === id;
+  });
+  if (foundStudent) {
+    return foundStudent;
+  } else {
+    return null;
+  }
+}
+const student = findStudentById(studentsWithAverage, 4);
+console.log(student);
